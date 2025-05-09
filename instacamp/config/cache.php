93fac9713202b6b9a91e249b7c0458da2a641d30
yisus82 <djalma8@gmail.com>
2025-05-09 12:17:26 +0200

    Connect to MongoDB

--- instacamp/config/cache.php
+++ instacamp/config/cache.php
@@ -90,6 +90,16 @@ return [
             'driver' => 'octane',
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'connection' => 'mongodb',
+            'collection' => 'cache',
+            'lock_connection' => 'mongodb',
+            'lock_collection' => 'cache_locks',
+            'lock_lottery' => [2, 100],
+            'lock_timeout' => 86400,
+        ],
+
     ],
 
     /*
--- instacamp/config/database.php
+++ instacamp/config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,6 +30,11 @@ return [
     */
 
     'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI'),
+            'database' => env('DB_DATABASE', 'instagram-clone'),
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
